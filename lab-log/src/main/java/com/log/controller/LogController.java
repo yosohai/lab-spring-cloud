478391fe2d3366bf6d32925e8c7d73fe449d07b6
lzqno186 <lzqno186@126.com>
2022-11-19 22:12:05 +0800

    feat:映射诊断上下文（MDC）

--- lab-log/src/main/java/com/log/controller/LogController.java
+++ lab-log/src/main/java/com/log/controller/LogController.java
@@ -14,10 +14,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @RestController
