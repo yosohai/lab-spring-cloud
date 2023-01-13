6e11ffc3996f72c64913e6bef3c59fcc9a90ff3b
lzqing <lzqing@chint.com>
2023-01-13 12:03:59 +0800

    feat:手写签名

--- lab-doc/src/main/java/com/lab/doc/controller/ThumbController.java
+++ lab-doc/src/main/java/com/lab/doc/controller/ThumbController.java
@@ -1,7 +1,6 @@
 package com.lab.doc.controller;
 
 import cn.hutool.core.util.StrUtil;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.lab.doc.utils.ImageUtils;
 import net.coobird.thumbnailator.Thumbnails;
 import org.apache.commons.io.FileUtils;
@@ -20,7 +19,6 @@ import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.util.HashMap;
 import java.util.UUID;
 
 @Controller
@@ -32,16 +30,9 @@ public class ThumbController {
 
     @RequestMapping(value = "/thumbnails")
     public void thumbnails(@RequestParam("thumb") String thumb, HttpServletRequest request, HttpServletResponse response) throws Exception {
-        HashMap<String, Object> hashMap = new HashMap<>();
-        ObjectMapper mapper = new ObjectMapper();
         if (StrUtil.isBlank(thumb)) {
             throw new Exception("上传失败，上传图片数据为空");
         }
-
-//        String imageData = request.getParameter("file");
-//        if (imageData.startsWith("data:image")) {
-//            imageData = imageData.split(",")[1];
-//        }
 
         String[] strArr = thumb.split("base64,");
         if (strArr == null || strArr.length != 2) {
@@ -74,22 +65,79 @@ public class ThumbController {
 
         //因为BASE64Decoder的jar问题，此处使用spring框架提供的工具包
         byte[] bytes = Base64Utils.decodeFromString(strArr[1]);
-        ByteArrayInputStream inputStream = new ByteArrayInputStream(bytes);
-        BufferedImage bufferedImage = ImageIO.read(inputStream);
-        Thumbnails.Builder<BufferedImage> builder = Thumbnails.of(bufferedImage)
-                .size(150, 80)//尺寸
-                .outputQuality(0.8f);//缩略图质量
+        try (ByteArrayInputStream inputStream = new ByteArrayInputStream(bytes)) {
+            BufferedImage bufferedImage = ImageIO.read(inputStream);
+            Thumbnails.Builder<BufferedImage> builder = Thumbnails.of(bufferedImage)
+                    .size(150, 70)//尺寸
+                    .outputQuality(1.0f);//缩略图质量
 
-        BufferedImage image = builder.asBufferedImage();
-        FileUtils.writeByteArrayToFile(new File(SAVE_PATH_IMAGES + tempFileName), ImageUtils.bufferedImageToBytes(image));
-        /*--------------------------------------------------------------------------------------------------------------------------------------------------------*/
-        //点击其他按键请求
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();//io流
-        ImageIO.write(image, "png", baos);//写入流中
-        byte[] bytes2 = baos.toByteArray();//转换成字节
-        BASE64Encoder encoder = new BASE64Encoder();
-        String png_base64 = encoder.encodeBuffer(bytes2);//转换成base64串
-        png_base64 = png_base64.replaceAll("\n", "").replaceAll("\r", "");//删除 \r\n
-        response.getWriter().write(png_base64);
+            BufferedImage image = builder.asBufferedImage();
+            FileUtils.writeByteArrayToFile(new File(SAVE_PATH_IMAGES + tempFileName), ImageUtils.bufferedImageToBytes(image));
+            /*--------------------上面保存到磁盘，下面返回缩略图流数据-----------------------------------*/
+//            String smallerBase64 = new BASE64Encoder().encodeBuffer(ImageUtils.bufferedImageToBytes(image));//转换成base64串
+//            smallerBase64 = smallerBase64.replaceAll("\n", "").replaceAll("\r", "");//删除 \r\n
+
+            String smallerBase64 = Base64Utils.encodeToString(ImageUtils.bufferedImageToBytes(image));
+            response.getWriter().write(smallerBase64);
+        } catch (Exception e) {
+            logger.error("生成签名缩略图失败", e);
+        }
+    }
+
+    @RequestMapping(value = "/thumbnails2")
+    public void thumbnails2(@RequestParam("thumb") String thumb, HttpServletRequest request, HttpServletResponse response) throws Exception {
+        if (StrUtil.isBlank(thumb)) {
+            throw new Exception("上传失败，上传图片数据为空");
+        }
+
+        String[] strArr = thumb.split("base64,");
+        if (strArr == null || strArr.length != 2) {
+            throw new Exception("上传失败，数据不合法");
+        }
+
+        logger.debug("对数据进行解析，获取文件名和流数据");
+        String suffix = "", dataPrix = strArr[0];
+        if ("data:image/jpeg;".equalsIgnoreCase(dataPrix)) {//data:image/jpeg;base64,base64编码的jpeg图片数据
+            suffix = ".jpg";
+        } else if ("data:image/x-icon;".equalsIgnoreCase(dataPrix)) {//data:image/x-icon;base64,base64编码的icon图片数据
+            suffix = ".ico";
+        } else if ("data:image/gif;".equalsIgnoreCase(dataPrix)) {//data:image/gif;base64,base64编码的gif图片数据
+            suffix = ".gif";
+        } else if ("data:image/png;".equalsIgnoreCase(dataPrix)) {//data:image/png;base64,base64编码的png图片数据
+            suffix = ".png";
+        } else {
+            throw new Exception("上传图片格式不合法");
+        }
+
+        File file = new File(SAVE_PATH_IMAGES);
+        if (!file.exists()) {
+            file.setWritable(true);
+            file.mkdirs();
+        }
+
+        String imgName = UUID.randomUUID().toString();
+        String tempFileName = imgName + suffix;
+        logger.debug("生成文件名为：" + tempFileName);
+
+        //因为BASE64Decoder的jar问题，此处使用spring框架提供的工具包
+        byte[] bytes = Base64Utils.decodeFromString(strArr[1]);
+        try (ByteArrayInputStream inputStream = new ByteArrayInputStream(bytes); ByteArrayOutputStream outputStream = new ByteArrayOutputStream();) {
+            BufferedImage bufferedImage = ImageIO.read(inputStream);
+            Thumbnails.Builder<BufferedImage> builder = Thumbnails.of(bufferedImage)
+                    .size(150, 70)//尺寸
+                    .outputQuality(1.0f);//缩略图质量
+
+            BufferedImage image = builder.asBufferedImage();
+            FileUtils.writeByteArrayToFile(new File(SAVE_PATH_IMAGES + tempFileName), ImageUtils.bufferedImageToBytes(image));
+            /*--------------------上面保存到磁盘，下面返回缩略图流数据-----------------------------------*/
+            ImageIO.write(image, "png", outputStream);//写入流中
+            byte[] smallerByte = outputStream.toByteArray();//转换成字节
+            String smallerBase64 = new BASE64Encoder().encodeBuffer(smallerByte);//转换成base64串
+            smallerBase64 = smallerBase64.replaceAll("\n", "").replaceAll("\r", "");//删除 \r\n
+//            response.getWriter().write(smallerBase64);
+            response.getWriter().write(strArr[1]);
+        } catch (Exception e) {
+
+        }
     }
 }
